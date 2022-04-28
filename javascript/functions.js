d602fff4c52d57336d5828e0af4db24d698e3823
andrewadi24 <66150348+andrewadi24@users.noreply.github.com>
2022-04-28 17:01:23 +1000

    Add files via upload
    Added hamburger menu functionality

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,10 +1,8 @@
-var images = [];
-images.push("https://drive.google.com/uc?export=view&id=1HmyRd1UBlxOt0S_LE5LWq7-W7bEMAkFA")
-images.push("")
-setTimeout("changeImage()", 30000);
-var x = 0;
 
-function changeImage() {
-    document.getElementById("img").src=images[x];
-    x++;
-}
+//Hamburger menu
+document.getElementById("hamburger1").addEventListener("click", function(){
+    document.getElementById("mobile-nav1").classList.remove("hidden");
+})
+document.getElementById("close1").addEventListener("click", function(){
+    document.getElementById("mobile-nav1").classList.add("hidden");
+})
